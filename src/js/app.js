7781c07e1eab3e240c6e1614354b3842d0b008ad
Maksym <maks@coderiver.com.ua>
2017-11-01 10:48:08 +0200

    default

--- src/js/app.js
+++ src/js/app.js
@@ -1,12 +1,11 @@
-
 import $ from 'jquery';
 
 $(document).ready(function() {
 
   $(window).bind('hashchange', function() {
-    $('.preview__item').hide();
+    $('.preview__item').removeClass('is-active');
     var block = window.location.hash;
-    $(block).show();
+    $(block).addClass('is-active');
     $('.tab__link').removeClass('is-active');
     $(`[class="tab__link"][href="${block}"]`).addClass('is-active');
   });
