37517a13d5dc328117b04b2dc4aa763c432b51d0
Maksym <maks@coderiver.com.ua>
2017-11-01 10:34:13 +0200

    done

--- src/js/app.js
+++ src/js/app.js
@@ -3,20 +3,12 @@ import $ from 'jquery';
 
 $(document).ready(function() {
 
-  $(window).bind('hashchange', function() { 
+  $(window).bind('hashchange', function() {
     $('.preview__item').hide();
     var block = window.location.hash;
     $(block).show();
-  });
-  $('.tab__link').on('click', function(e) {
-    if (!$(this).hasClass('is-active')) {
-      $('.tab__link').removeClass('is-active');
-      
-      $(this).addClass('is-active');
-    } else {
-      $(this).removeClass('is-active');
-    }
-    
+    $('.tab__link').removeClass('is-active');
+    $(`[class="tab__link"][href="${block}"]`).addClass('is-active');
   });
 });
 
